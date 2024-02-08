42cb80cc3d93a025bd1b3067bffd766bd799e127
Chris <frishkorn@frishkorn.com>
2024-02-08 23:38:20 +0000

    updated plot_blockchain

--- plot_blockchain.py
+++ plot_blockchain.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         data = json.load(json_file)
 
     # Create a folium map centered at the initial location
-    map = folium.Map(location=[0, 0], tiles="CartoDB_Positron", attr="OpenStreetMap", zoom_start=2)
+    map = folium.Map(location=[0, 0], tiles="OpenStreetMap", attr="OpenStreetMap", zoom_start=2)
 
     # Plot only the last 100 ISS locations on the map
     for i, entry in enumerate(data[-100:]):  # Iterate over the last 100 entries (~1-1/2 hr).
